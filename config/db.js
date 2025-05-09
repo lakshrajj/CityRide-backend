805126bc7aa519652774415e6ad746bd7a529b44
Luxx <70052551+lakshrajj@users.noreply.github.com>
2025-05-09 22:11:59 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/carpooling',
+  dbOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+};
